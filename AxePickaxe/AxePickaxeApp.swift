ad7211115633be50c9ddad0f3ec1aa338db76375
TaumuLu <972409545@qq.com>
2024-03-17 22:35:34 +0800

    Initial Commit

---
+++ AxePickaxe/AxePickaxeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AxePickaxeApp.swift
+//  AxePickaxe
+//
+//  Created by Taumu Lu on 2024/3/17.
+//
+
+import SwiftUI
+
+@main
+struct AxePickaxeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AxePickaxe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AxePickaxe
+//
+//  Created by Taumu Lu on 2024/3/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
